7de3b6f511ff337810cfc33c534be0757d8cc177
Mesaque Txangala <nikimanuel15@gmail.com>
2025-01-30 13:42:57 +0100

    Cria arquivos de stilos e estilisa o título.

---
+++ src/components/title/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    textTitle: {
+        color: "#FF0043",
+        fontSize:24,
+        fontWeight:"bold",
+    },
+    boxTitle: {
+        alignItems:"center",
+        justifyContent:"center",
+        padding:10
+    }
+});
+
+export default styles
