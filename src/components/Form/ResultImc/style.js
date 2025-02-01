27d05e578d3a311f7a421409e39d138cc6b2f7be
Mesaque Txangala <nikimanuel15@gmail.com>
2025-02-01 15:11:36 +0100

    Estilisa a secção do resultado.

---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    contextImc: {
+        flex:1,
+        marginTop:15,
+        paddingTop:10,
+        borderRadius:50,
+        aligntItems:"center",
+    },
+    information: {
+        fontSize:18,
+        color: "#FF0043",
+        fontWeight:"bold",
+    },
+    numberImc: {
+        fontSize: 18,
+        color: "#FF0043",
+        fontWeight:"bold",
+    }
+});
+
+export default styles;
